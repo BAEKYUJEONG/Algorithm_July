ecab2baf199796a20321f3f9d594aa604eb293aa
YuJeong <qordbwjd96@gmail.com>
2020-07-23 23:55:01 +0900

    제출

--- 0723/hwjava04_seoul_class8_baekyujeong/ProductTest.java
+++ 0723/hwjava04_seoul_class8_baekyujeong/ProductTest.java
@@ -1,7 +1,7 @@
 package hwjava04_seoul_class8_baekyujeong;
 
 public class ProductTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		
 		TV tv = new TV("KQ65QT950SFXKR","QLED 8K 163cm Infinity Screen",9280000,9999,65,"QLED");
 		Refrigerator rf = new Refrigerator("RF10T9965APS","셰프컬렉션 900 L 다크 크롬 프레임",11980000,9999,900);
